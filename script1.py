12d014a4033a5e03af044f0e471d1c216b94e956
rupali007 <75978067+rupali007@users.noreply.github.com>
2021-06-13 16:53:38 +0530

    Add files via upload

--- script1.py
+++ script1.py
@@ -8,4 +8,20 @@ for num in range(start, end + 1):
         print(num, end = " ")
         
         
-        C:\Windows\System32\nodePythonApp>
+        --------------
+        str="L39u37x35P33M31s29o27f25t";
+        def splitString(str):
+ 
+    alpha = ""
+    num = ""
+    
+    for i in range(len(str)):
+        if (str[i].isdigit()):
+            num = num+ str[i]
+        elif((str[i] >= 'A' and str[i] <= 'Z') or
+             (str[i] >= 'a' and str[i] <= 'z')):
+            alpha += str[i]
+       
+    print(alpha)
+    print(num )
+    
